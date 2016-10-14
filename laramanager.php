6f720b1973a00ddc3a70822ef4113fdcd4adfb40
hakob90 <hakob93baghdasaryan@gmail.com>
2016-10-15 00:01:54 +0400

    refactor to class

--- laramanager.php
+++ laramanager.php
@@ -1,24 +1,13 @@
 #! /usr/bin/env php
 
 <?php
-
+use App\SayHelloCommand;
 use Symfony\Component\Console\Application;
-use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputArgument;
-use Symfony\Component\Console\Output\OutputInterface;
 
 require 'vendor/autoload.php';
 
 $app = new Application('Laramanger', '1.0');
 
-$app->register('sayHelloTo')
-    ->setDescription('Offer a greeting to the given person')
-    ->addArgument('name', InputArgument::REQUIRED)
-    ->setCode(function (InputInterface $input, OutputInterface $output){
-        $name = $input->getArgument('name');
-        $message = 'Hello, ' . $name . '!';
-
-        $output->writeln("<info>{$message}<info>");
-    });
+$app->add(new SayHelloCommand());
 
 $app->run();

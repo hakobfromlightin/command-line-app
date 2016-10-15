b29df21f73f76cce234f0fb415480b1f095516df
hakob90 <hakob93baghdasaryan@gmail.com>
2016-10-15 13:43:40 +0400

    laravel install

--- laramanager.php
+++ laramanager.php
@@ -1,7 +1,9 @@
 #! /usr/bin/env php
 
 <?php
+use App\NewCommand;
 use App\SayHelloCommand;
+use GuzzleHttp\Client;
 use Symfony\Component\Console\Application;
 
 require 'vendor/autoload.php';
@@ -9,5 +11,6 @@ require 'vendor/autoload.php';
 $app = new Application('Laramanger', '1.0');
 
 $app->add(new SayHelloCommand());
+$app->add(new NewCommand(new Client()));
 
 $app->run();
